be1e673149febdeda22365a33e7f5215edba44b2
slanda156 <slandalp@gmail.com>
2024-04-20 00:25:59 +0200

    Added tests for database

---
+++ tests/test_database.py
@@ -0,0 +1,41 @@
+from src.database import Database
+from src.component import Component
+from src.location import Location
+
+db = Database()
+
+def test_connectDb():
+    assert db.connect() is True
+
+def test_createComponent():
+    component = Component("Test", description="This is a test component", price=1.0, imagePath="test.png", datasheetPath="test.pdf")
+    assert db.createComponent(component) is True
+
+def test_getComponents():
+    assert db.getComponents() is not None
+
+def test_createLocation():
+    location = Location("Test", parentID=-1, shortName="T", description="This is a test location")
+    assert db.createLocation(location) is True
+
+def test_getLocations():
+    assert db.getLocations() is not None
+
+def test_createComponentLocationMap():
+    assert db.createComponentLocationMap(1, 1, 5) is True
+
+def test_addComponentToLocation():
+    assert db.addComponentToLocation(1, 1, 1) is True
+
+def test_removeComponentFromLocation():
+    assert db.removeComponentFromLocation(1, 1, 6) is True
+
+def test_deleteComponent():
+    component = db.getComponent(name="Test")
+    assert component is not None
+    assert db.deleteComponent(component) is True
+
+def test_deleteLocation():
+    location = db.getLocation(name="Test")
+    assert location is not None
+    assert db.deleteLocation(location) is True
