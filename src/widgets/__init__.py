6b5441bdfd687c2dde099eb3313cc8716a303453
slanda156 <slandalp@gmail.com>
2024-04-28 11:20:37 +0200

    Initial Creation

--- src/widgets/__init__.py
+++
@@ -1,8 +0,0 @@
-from .componentInfo import ComponentInfo  # noqa: F401
-from .locationInfo import LocationInfo  # noqa: F401
-from .popups import AddComponentLocation  # noqa: F401
-from .popups import AddComponent  # noqa: F401
-from .popups import AddLocation  # noqa: F401
-from .popups import RemoveComponentLocation  # noqa: F401
-from .popups import RemoveComponent  # noqa: F401
-from .popups import RemoveLocation  # noqa: F401
