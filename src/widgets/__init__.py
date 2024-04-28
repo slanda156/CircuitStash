d63725560e1203bc1106f34910e48409306ed5ac
slanda156 <slandalp@gmail.com>
2024-04-28 10:28:07 +0200

    aded all the new widgets

--- src/widgets/__init__.py
+++ src/widgets/__init__.py
@@ -1,1 +1,8 @@
-from .treeView import TreeView  # noqa: F401
+from .componentInfo import ComponentInfo  # noqa: F401
+from .locationInfo import LocationInfo  # noqa: F401
+from .popups import AddComponentLocation  # noqa: F401
+from .popups import AddComponent  # noqa: F401
+from .popups import AddLocation  # noqa: F401
+from .popups import RemoveComponentLocation  # noqa: F401
+from .popups import RemoveComponent  # noqa: F401
+from .popups import RemoveLocation  # noqa: F401
