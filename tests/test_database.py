a80b3447136022cde81fc5fa9375c65df296db68
slanda156 <slandalp@gmail.com>
2024-04-29 14:06:59 +0200

    Fixed some tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -2,10 +2,20 @@ from src.database import Database
 from src.component import Component
 from src.location import Location
 
-db = Database()
+
+db = Database("sqlite:///data/test_database.db")
+
 
 def test_connectDb():
     assert db.connect() is True
+    allComponents = db.getComponents()
+    for component in allComponents:
+        db.deleteComponent(component, force=True)
+    assert db.getComponents() == []
+    allLocations = db.getLocations()
+    for location in allLocations:
+        db.deleteLocation(location, force=True)
+    assert db.getLocations() == []
 
 def test_createComponent():
     component = Component("Test", description="This is a test component", price=1.0)
@@ -23,17 +33,21 @@ def test_getLocations():
 
 def test_createComponentLocationMap():
     assert db.createComponentLocationMap(1, 1, 5) is True
+    assert db.getComponentAmountInLocation(1, 1) == 5
 
 def test_addComponentToLocation():
     assert db.addComponentToLocation(1, 1, 1) is True
+    assert db.getComponentAmountInLocation(1, 1) == 6
 
 def test_removeComponentFromLocation():
-    assert db.removeComponentFromLocation(1, 1, 6) is True
+    assert db.removeComponentFromLocation(1, 1, 5) is True
+    assert db.getComponentAmountInLocation(1, 1) == 1
 
 def test_deleteComponent():
     component = db.getComponent(name="Test")
     assert component is not None
-    assert db.deleteComponent(component) is True
+    assert db.deleteComponent(component) is False
+    assert db.deleteComponent(component, force=True) is True
 
 def test_deleteLocation():
     location = db.getLocation(name="Test")
