a280aede1dae79d595935bd71395206588fa42f4
slanda156 <slandalp@gmail.com>
2024-05-25 16:59:56 +0200

    added exceptoins handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yaml
 import logging
 import logging.config
 from logging import getLogger
+from traceback import format_exc
 
 from src.app import App
 
@@ -12,9 +13,16 @@ with open("logger.yaml") as f:
 
 logger = getLogger(__name__)
 
-if __name__ == "__main__":
+def main() -> None:
     logger.info("Starting Circuit Stash")
     app = App()
     app.init()
     app.mainloop()
     logger.info("Closing Circuit Stash\n"+ 96* "-")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        logger.error(format_exc())
+        raise e
